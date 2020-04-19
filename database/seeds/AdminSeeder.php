f9fb05ca0a21898e50aa050500a36fa5c7f9ce91
Kevlon <kevlon@kevlons-macbook.home>
2020-04-19 13:26:01 -0700

    add logins for testing - seeds

--- database/seeds/AdminSeeder.php
+++ database/seeds/AdminSeeder.php
@@ -17,13 +17,13 @@ class AdminSeeder extends Seeder
                 'password' => '$2y$10$du.7Yc9sw6kBQBpwUQUwCO6NGgA9xFxZ4TFPeR6SaDgUeYDR35HOi'],
             ['name'    => 'Kevlon',
                 'email'    => 'kgalloway56@yahoo.com',
-                'password' => '$2y$10$du.7Yc9sw6kBQBpwUQUwCO6NGgA9xFxZ4TFPeR6SaDgUeYDR35HOi'],
+                'password' => '$2y$10$QFkedhHJRYQjkOl4P0g59e8zvdSf0MFx.dFKv8ZF9DTXSP90LzUmy'],
             ['name'    => 'Kevin Cates',
                 'email'    => 'kcates@koolriculum.com',
-                'password' => '$2y$10$vFYUul.3lNVU262R5cOv8u.PakPQ7QNc7jEI8A7guMKelp5cxG9WW'],
+                'password' => '$2y$10$QFkedhHJRYQjkOl4P0g59e8zvdSf0MFx.dFKv8ZF9DTXSP90LzUmy'],
             ['name'    => 'Kevia Cates',
                 'email'    => 'keviacates@koolriculum.com',
-                'password' => '$2y$10$vFYUul.3lNVU262R5cOv8u.PakPQ7QNc7jEI8A7guMKelp5cxG9WW'],
+                'password' => '$2y$10$QFkedhHJRYQjkOl4P0g59e8zvdSf0MFx.dFKv8ZF9DTXSP90LzUmy'],
         ]);
 
         DB::table('users')->insert([
@@ -33,22 +33,22 @@ class AdminSeeder extends Seeder
                  'is_active' => 1],
             ['name'    => 'Kevlon',
                 'email'    => 'kgalloway56@yahoo.com',
-                'password' => '$2y$10$du.7Yc9sw6kBQBpwUQUwCO6NGgA9xFxZ4TFPeR6SaDgUeYDR35HOi',
+                'password' => '$2y$10$QFkedhHJRYQjkOl4P0g59e8zvdSf0MFx.dFKv8ZF9DTXSP90LzUmy',
             'is_active' => 1],
             ['name'    => 'Kevin Cates',
                 'email'    => 'kcates@koolriculum.com',
-                'password' => '$2y$10$vFYUul.3lNVU262R5cOv8u.PakPQ7QNc7jEI8A7guMKelp5cxG9WW',
+                'password' => '$2y$10$QFkedhHJRYQjkOl4P0g59e8zvdSf0MFx.dFKv8ZF9DTXSP90LzUmy',
             'is_active' => 1],
             ['name'    => 'Kevia Cates',
                 'email'    => 'keviacates@koolriculum.com',
-                'password' => '$2y$10$vFYUul.3lNVU262R5cOv8u.PakPQ7QNc7jEI8A7guMKelp5cxG9WW',
+                'password' => '$2y$10$QFkedhHJRYQjkOl4P0g59e8zvdSf0MFx.dFKv8ZF9DTXSP90LzUmy',
             'is_active' => 1],
         ]);
 
         DB::table('teachers')->insert([
             ['name'    => 'Kevlon Jr',
                 'email'    => 'kevinwoods@gmail.com',
-                'password' => '$2y$10$du.7Yc9sw6kBQBpwUQUwCO6NGgA9xFxZ4TFPeR6SaDgUeYDR35HOi',
+                'password' => '$2y$10$QFkedhHJRYQjkOl4P0g59e8zvdSf0MFx.dFKv8ZF9DTXSP90LzUmy',
                 'organization_id' => 1]
         ]);
     }
